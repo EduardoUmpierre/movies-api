83a41d68bb49745641f5190c680c83c01c48b3e4
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-12-04 08:05:33 -0200

    Port fix

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
 func main() {
+    addr, err := determineListenAddress()
+    if err != nil {
+        log.Fatal(err)
+    }
+
     a := App{}
     a.Initialize("root", "", "movies-api")
-    a.Run(":8080")
+    a.Run(addr)
+}
+
+func determineListenAddress() (string, error) {
+    port := os.Getenv("PORT")
+    if port == "" {
+        return "", fmt.Errorf("$PORT not set")
+    }
+    return ":" + port, nil
 }
